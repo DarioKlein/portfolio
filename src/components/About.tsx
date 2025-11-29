4df795940d48daba3b5a6343c5a05785eb03ee8c
Dario Klein <dariokleinalves@gmail.com>
2025-11-29 01:36:16 -0300

    fix mobile bug

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,13 +2,13 @@ import imgProfile from '../assets/avatar.jpg'
 
 export function About() {
   return (
-    <section className="w-full h-screen bg-[var(--color-background-about)] flex">
-      <div className="w-[50%] h-full flex justify-center items-center">
-        <div className="w-100 h-100 overflow-hidden rounded-full border-4 border-[var(--color-background-01)]">
+    <section className="w-full md:h-screen bg-[var(--color-background-about)] flex flex-col md:flex-row pt-16 pb-16 gap-20 md:pt-0 md:pb-0 md:gap-0">
+      <div className="w-full h-full flex justify-center items-center md:w-[50%]">
+        <div className="md:w-100 md:h-100 w-80 h-80 overflow-hidden rounded-full border-4 border-[var(--color-background-01)]">
           <img src={imgProfile} className=" rounded-full w-full h-full scale-120" alt="" />
         </div>
       </div>
-      <div className="flex justify-center w-[50%] font-[var(--font-sans)] pr-75 flex-col gap-4">
+      <div className="flex justify-center w-full md:w-[50%] font-[var(--font-sans)] md:pr-75 px-10 flex-col gap-4">
         <h2 className="text-5xl font-bold">Sobre Mim</h2>
         <p className="text-[18px] text-[var(--subtitle-color-font)]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vel placeat in delectus ipsam laborum
